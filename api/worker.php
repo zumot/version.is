21d66236447946b17c27358c602b1a8203ed7996
Gustav Nikolaj <gustavnikolaj@gmail.com>
2012-09-10 15:32:49 +0200

    working on adding support for other formats than package.json

--- api/worker.php
+++ api/worker.php
@@ -5,14 +5,20 @@ $versions = array();
 $sources = json_decode(file_get_contents('version_sources.json'), true);
 
 foreach ($sources as $project => $source) {
-  $info = json_decode(file_get_contents($source), true);
   $version = 'N/A';
 
   $filename = end(explode('/', $source));
+  
 
   if ($filename == 'package.json') {
+    $info = json_decode(file_get_contents($source), true);
     $version = $info['version'];
   }
+
+  if ($filename == 'grunt.js') {
+    // Hmm..
+  }
+
   $versions[$project] = $version;
 }
 
