9ee73b96e2eefccef4929530ce1cd2ce5b4a5430
Gustav Nikolaj <gustavnikolaj@gmail.com>
2012-09-10 12:41:08 +0200

    lol

--- api/index.php
+++ api/index.php
@@ -7,12 +7,12 @@ $cache_buster = floor(time() / 100) * 100;
 
 echo $url['path'][0];
 
-//$versions = apc_fetch('versions.json:'.$cache_buster);
+$versions = apc_fetch('versions.json:'.$cache_buster);
 
 if (!$versions) {
   echo 'get content' . PHP_EOL;
   $versions = json_decode(file_get_contents('versions.json'), true);
-//  apc_store('versions.json:'.$cache_buster, $versions);
+  apc_store('versions.json:'.$cache_buster, $versions);
 }
 
 $project = $url['path'][0];
