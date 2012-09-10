ad28fdc3e0bba61e1a234e290a559b7c9171b628
Gustav Nikolaj <gustavnikolaj@gmail.com>
2012-09-10 13:27:40 +0200

    API Working

--- api/index.php
+++ api/index.php
@@ -1,26 +1,31 @@
 <?php
 header("Content-Type: application/json");
-// Getting the URL
+
 $url = parse_url($_SERVER['REQUEST_URI']);
-// Exploding the path
-$url['path'] = explode('/', ltrim($url['path'], '/'));
-// Parsing the querystring
-parse_str($url['query'], $url['query']);
+$url['path'] = explode('/', ltrim($url['path'], '/')); // path to array
+
+$project = $url['path'][0]; // Get project name
+
+// if querystring is not empty check for a callback
+if ($url['query']) {
+  parse_str($url['query'], $url['query']);
+  $callback = filter_var($url['query']['callback'], FILTER_SANITIZE_STRING);
+} else {
+  $callback = false;
+}
 
 // Cache buster to refresh APC cache every 100 secs.
-$cache_buster = floor(time() / 100) * 100;
+$cacheTTL = floor(time() / 100) * 100;
 
 // Try to fetch from APC
-$versions = apc_fetch('versions.json'.$cache_buster);
+$versions = apc_fetch('versions.json'.$cacheTTL);
 
 // If not in cache - then fetch and store
 if (!$versions) {
   $versions = json_decode(file_get_contents('versions.json'), true);
-  apc_store('versions.json'.$cache_buster, $versions);
+  apc_store('versions.json'.$cacheTTL, $versions);
 }
 
-// Project key
-$project = $url['path'][0];
 
 // If has data
 if (isset($versions[$project])) {
