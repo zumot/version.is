88f181a94f8a31a911dda3b0d943ef21dd914dfd
Gustav Nikolaj <gustavnikolaj@gmail.com>
2012-09-10 15:07:05 +0200

    Adding worker process

---
+++ api/worker.php
@@ -0,0 +1,13 @@
+<?php
+
+$versions = array();
+
+$sources = json_decode(file_get_contents('version_sources.json'), true);
+
+foreach ($sources as $project => $source) {
+  $info = json_decode(file_get_contents($source), true);
+
+  $versions[$project] = $info['version'];
+}
+
+file_put_contents('versions.json', json_encode($versions));
