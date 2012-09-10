1cb3be2af0d91f254a0e135dcb55dac1fe376295
Gustav Nikolaj <gustavnikolaj@gmail.com>
2012-09-10 15:29:36 +0200

    working on adding support for other formats than package.json

--- api/worker.php
+++ api/worker.php
@@ -6,10 +6,17 @@ $sources = json_decode(file_get_contents('version_sources.json'), true);
 
 foreach ($sources as $project => $source) {
   $info = json_decode(file_get_contents($source), true);
+  $version = 'N/A';
 
-  $versions[$project] = $info['version'];
+  $filename = end(explode('/', $source));
+
+  if ($filename == 'package.json') {
+    $version = $info['version'];
+  }
+  $versions[$project] = $version;
 }
 
 $result = json_encode($versions);
 file_put_contents('versions.json', $result);
+
 echo '<pre>'.$result;
