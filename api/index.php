522110e94c5cda7d1b88e482b4addd77f8559830
Gustav Nikolaj <gustavnikolaj@gmail.com>
2012-09-10 12:11:08 +0200

    Initial commit

--- api/index.php
+++ api/index.php
@@ -1,8 +1,18 @@
-<?php
+<pre><?php
+$url = parse_url($_SERVER['REQUEST_URI']);
 
-$response = array(
-  'message' => 'hello world!',
-  'uri' => $_SERVER['REQUEST_URI']
-);
+print_r($url);
+
+echo PHP_EOL;
+
+$cache_buster = number_format(time() / 100, 0) * 100;
+
+echo $cache_buster . PHP_EOL;
+
+$versions = apc_fetch('versions.json:'.$cache_buster);
+
+if (!$versions) {
+  echo 'get content' . PHP_EOL;
+}
 
 echo json_encode($response);
