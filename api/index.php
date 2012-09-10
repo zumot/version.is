8cfd10deef013ddcbbf4607fcaa67d0680a86832
Gustav Nikolaj <gustavnikolaj@gmail.com>
2012-09-10 16:39:17 +0200

    Cleaning up the file + additional comments.

--- api/index.php
+++ api/index.php
@@ -1,17 +1,30 @@
 <?php
+/******************************************************************************
+ * URL Parsing                                                                *
+ ******************************************************************************/
 $url = parse_url($_SERVER['REQUEST_URI']);
 $url['path'] = explode('/', ltrim($url['path'], '/')); // path to array
 
 $project = $url['path'][0]; // Get project name
-$callback = false; // Use JSONP-style callback
-$format = 'plaintext';
+$callback = false;          // Use JSONP-style callback?
+$format = 'plaintext';      // Default format
 
 // if querystring is not empty check for a callback and format
 if ($url['query']) {
   parse_str($url['query'], $url['query']);
   $callback = filter_var($url['query']['callback'], FILTER_SANITIZE_STRING);
-  $format = filter_var(strtolower($url['query']['format']), FILTER_SANITIZE_STRING);
+  $format = filter_var(strtolower($url['query']['format']),
+                       FILTER_SANITIZE_STRING);
 }
+
+/* TODO:
+ * Use $_SERVER['HTTP_ACCEPT'] to choose a meaningful format.
+ * ref: https://github.com/h5bp/lazyweb-requests/issues/96#issuecomment-8423026
+ */
+
+/******************************************************************************
+ * Getting version info. Reading from APC cache if it exists.                 *
+ ******************************************************************************/
 
 // Cache buster to refresh APC cache every 100 secs.
 $cacheTTL = floor(time() / 100) * 100;
@@ -24,6 +37,10 @@ if (!$versions) {
   $versions = json_decode(file_get_contents('versions.json'), true);
   apc_store('versions.json'.$cacheTTL, $versions);
 }
+
+/******************************************************************************
+ * Preparing a response                                                       *
+ ******************************************************************************/
 
 // If a project is asked for...
 if ($project) {
@@ -43,6 +60,9 @@ if ($project) {
 }
 
 
+/******************************************************************************
+ * Returning the proper response                                              *
+ ******************************************************************************/
 if ($format == 'json') {
   header("Content-Type: application/json");
 
