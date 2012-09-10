c89d1ebe9d79faf51ce00cf8f396fc35ce7d9712
Gustav Nikolaj <gustavnikolaj@gmail.com>
2012-09-10 12:40:57 +0200

    lol

--- api/index.php
+++ api/index.php
@@ -7,23 +7,23 @@ $cache_buster = floor(time() / 100) * 100;
 
 echo $url['path'][0];
 
-$versions = apc_fetch('versions.json:'.$cache_buster);
+//$versions = apc_fetch('versions.json:'.$cache_buster);
 
 if (!$versions) {
   echo 'get content' . PHP_EOL;
-  $versions = json_decode(file_get_contents('versions.json'));
-  apc_store('versions.json:'.$cache_buster, $versions);
+  $versions = json_decode(file_get_contents('versions.json'), true);
+//  apc_store('versions.json:'.$cache_buster, $versions);
 }
 
-if (isset($versions->{$url['path'][0]})) {
+$project = $url['path'][0];
 
-  $project = $url['path'][0];
-  $version = $versions->{'jquery'};
+if (isset($versions[$project])) {
+  $version = $versions[$project][0];
 
   $response = array( $project => $version );
 } else {
   $response = array(
-    'error' => 'No match found'
+    'error' => 'No match found for \''.$project.'\''
   );
 }
 
