1d71dc01dd6fa704829fbeea8979545e080102f4
Gustav Nikolaj <gustavnikolaj@gmail.com>
2012-09-10 12:26:01 +0200

    lol

--- api/index.php
+++ api/index.php
@@ -3,24 +3,18 @@ $url = parse_url($_SERVER['REQUEST_URI']);
 $url['path'] = explode('/', ltrim(rtrim($url['path'], '/'), '/'));
 parse_str($url['query'], $url['query']);
 
-print_r($url);
-
-echo PHP_EOL;
-
 $cache_buster = floor(time() / 100) * 100;
 
 echo $cache_buster . PHP_EOL;
 
-//$versions = apc_fetch('versions.json:'.$cache_buster);
+$versions = apc_fetch('versions.json:'.$cache_buster);
 
 if (!$versions) {
   echo 'get content' . PHP_EOL;
 
   $versions = json_decode(file_get_contents('versions.json'));
-  //apc_store('versions.json:'.$cache_buster, $versions);
+  apc_store('versions.json:'.$cache_buster, $versions);
 }
-
-print_r($versions);
 
 if (isset($versions->{$url[0]})) {
   $response = array(
