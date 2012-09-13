3a27d2887a86e2f20f63585bb722b091eeb02411
Gustav Nikolaj <gustavnikolaj@gmail.com>
2012-09-13 15:36:51 +0200

    Added custom index template for text/html hits

---
+++ app/views/index.py
@@ -0,0 +1,48 @@
+import json
+
+from google.appengine.ext import webapp
+from app.helpers import format, template
+
+
+class Index(webapp.RequestHandler):
+    def get(self):
+        response_format = format.get(self.request.get('format'), self.request.headers['accept'])
+
+        response = gimmeIndex(response_format, self.request.get('callback'))
+
+        self.response.status = response[1]
+        self.response.headers['Charset'] = 'utf-8'
+        self.response.headers['Content-Type'] = response_format
+
+        self.response.write(response[0])
+
+
+def gimmeIndex(response_format, callback):
+    formats = format.formats()
+
+    if response_format == formats[0]:
+        return indexHtml()
+    if response_format == formats[1]:
+        return indexPlain()
+    if response_format == formats[2]:
+        return indexJson(callback)
+
+
+def indexHtml():
+    return (template.render('index', {}), 200)
+
+
+def indexPlain():
+    return ('Invalid request', 400)
+
+
+def indexJson(callback):
+    content = {'message': 'Invalid Request'}
+
+    if callback != '':
+        content = json.dumps(content, separators=(',', ':'))
+        content = callback + '(' + content + ');'
+    else:
+        content = json.dumps(content, indent=2)
+
+    return (content, 400)
--- main.py
+++ main.py
@@ -1,21 +1,11 @@
 from google.appengine.ext import webapp
 from app.views.projects import Projects
 from app.views.projectversion import ProjectVersion
-
-
-charset = 'utf-8'
-
-
-class MainPage(webapp.RequestHandler):
-    def get(self):
-        self.response.headers['Charset'] = charset
-        self.response.headers['Content-Type'] = 'text/plain'
-        self.response.status = 400
-        self.response.write('Invalid request')
+from app.views.index import Index
 
 
 app = webapp.WSGIApplication([
-    webapp.Route('/', MainPage),
+    webapp.Route('/', Index),
     webapp.Route('/projects', Projects),
     webapp.Route('/<project:[a-z0-9-_](.*)>', ProjectVersion)
 ], debug=True)
