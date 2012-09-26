5088d071027a8b16bab4d7712ecc5924bb2e8ff2
Gustav Nikolaj <gustavnikolaj@gmail.com>
2012-09-26 17:18:06 +0200

    Added a message pointing to the list of monitored projects, as a fallback for browsers that do not support datalist. Ref: Issue #19

--- static/assets/js/script.js
+++ static/assets/js/script.js
@@ -1,6 +1,16 @@
+hasDatalist = function () {
+  return 'options' in document.createElement('datalist');
+};
+
 submitHandler = function (e) {
   window.location = '/' + document.getElementById('searchInput').value;
   e.preventDefault();
 };
 
 document.getElementById('searchForm').addEventListener("submit", submitHandler);
+
+if (!hasDatalist()) {
+  content = 'Refer to the <a href="/projects">list of monitored projects</a> '
+          + 'for a the project names.'
+  document.getElementById('searchTip').innerHTML = content;
+}
